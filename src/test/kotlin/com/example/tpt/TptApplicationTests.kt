8ce3ab4e49fb13b2f8d979b93d666da4f56a1c37
dong99u <qkrehdrb0813@gmail.com>
2025-07-04 00:18:54 +0900

    :tada: START

---
+++ src/test/kotlin/com/example/tpt/TptApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tpt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TptApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
