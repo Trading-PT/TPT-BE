dfa617e53282c57249c2740e61e0b2b0b1ee8966
이승주 <eun07213@gmail.com>
2025-09-05 00:49:11 +0900

    feat: 유저 관련 enums 추가

---
+++ src/main/java/com/tradingpt/tpt_api/user/enums/Provider.java
@@ -0,0 +1,5 @@
+package com.tradingpt.tpt_api.user.enums;
+
+public enum Provider {
+    LOCAL, KAKAO, NAVER
+}
---
+++ src/main/java/com/tradingpt/tpt_api/user/enums/Role.java
@@ -0,0 +1,5 @@
+package com.tradingpt.tpt_api.user.enums;
+
+public enum Role {
+    ROLE_ADMIN, ROLE_TRAINER, ROLE_CUSTOMER
+}
