d58d54e185239ad533d52255eb01d937711ddfcf
이승주 <eun07213@gmail.com>
2025-09-27 20:55:43 +0900

    feat: 유저 서비스 메서드 추가

--- src/main/java/com/tradingpt/tpt_api/domain/user/service/UserService.java
+++ src/main/java/com/tradingpt/tpt_api/domain/user/service/UserService.java
@@ -1,10 +1,13 @@
 package com.tradingpt.tpt_api.domain.user.service;
 
-import com.tradingpt.tpt_api.domain.auth.dto.response.FindIdResponse;
+import com.tradingpt.tpt_api.domain.user.dto.request.ChangePasswordRequest;
+import com.tradingpt.tpt_api.domain.user.dto.response.FindIdResponse;
 import com.tradingpt.tpt_api.domain.auth.dto.response.MeResponse;
 
 public interface UserService {
     void ensureUnique(String username, String email, String phone);
     FindIdResponse findUserId(String email);
     MeResponse getMe(Long userId);
+    void changePassword(Long userId, ChangePasswordRequest req);
+    void deleteAccount(Long userId);
 }
