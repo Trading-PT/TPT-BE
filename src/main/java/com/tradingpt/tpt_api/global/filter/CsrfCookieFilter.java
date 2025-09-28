b44f62ce11e81c4f7fe217528e0071b494a2073d
dong99u <qkrehdrb0813@gmail.com>
2025-09-28 15:36:23 +0900

    :sparkles: feat: XSRF-TOKEN 헤더 설정 로직 개선
    - `CsrfCookieFilter`에서 XSRF-TOKEN 값을 찾는 로직을 보완했습니다.
    - 헤더에 XSRF-TOKEN이 없을 경우 Set-Cookie 속성에서 값을 추출하도록 추가했습니다.

--- src/main/java/com/tradingpt/tpt_api/global/filter/CsrfCookieFilter.java
+++ src/main/java/com/tradingpt/tpt_api/global/filter/CsrfCookieFilter.java
@@ -18,16 +18,34 @@ public class CsrfCookieFilter extends OncePerRequestFilter {
 									 HttpServletResponse response,
 									 FilterChain filterChain) throws ServletException, IOException {
 
-		CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
-		if (csrfToken == null) {
-			csrfToken = (CsrfToken) request.getAttribute("_csrf");
-		}
-
-		if (csrfToken != null) {
-			String token = csrfToken.getToken(); // DeferredCsrfToken 초기화를 트리거
-			response.setHeader("XSRF-TOKEN", token);
-		}
-
 		filterChain.doFilter(request, response);
+
+		CsrfToken csrfToken = (CsrfToken)request.getAttribute(CsrfToken.class.getName());
+		if (csrfToken == null) {
+			csrfToken = (CsrfToken)request.getAttribute("_csrf");
+		}
+
+		String tokenValue = csrfToken != null ? csrfToken.getToken() : null;
+		if (tokenValue == null) {
+			for (String setCookie : response.getHeaders("Set-Cookie")) {
+				int index = setCookie.indexOf("XSRF-TOKEN=");
+				if (index < 0) {
+					continue;
+				}
+
+				String valuePortion = setCookie.substring(index + "XSRF-TOKEN=".length());
+				int semicolon = valuePortion.indexOf(';');
+				if (semicolon >= 0) {
+					valuePortion = valuePortion.substring(0, semicolon);
+				}
+
+				tokenValue = java.net.URLDecoder.decode(valuePortion, java.nio.charset.StandardCharsets.UTF_8);
+				break;
+			}
+		}
+
+		if (tokenValue != null) {
+			response.setHeader("XSRF-TOKEN", tokenValue);
+		}
 	}
 }
