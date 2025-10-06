0f7427da5590d92868feaa947b2f809358d40bec
이승주 <eun07213@gmail.com>
2025-10-06 19:54:56 +0900

    feat: 민원 작성 요청 및 응답 DTO

---
+++ src/main/java/com/tradingpt/tpt_api/domain/complaint/dto/request/CreateComplaintRequestDTO.java
@@ -0,0 +1,21 @@
+package com.tradingpt.tpt_api.domain.complaint.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CreateComplaintRequestDTO {
+
+    @Schema(description = "민원 제목", example = "8월 결제 환불 요청")
+    @NotBlank(message = "제목은 필수입니다.")
+    private String title;
+
+    @Schema(description = "민원 내용", example = "8월 결제 환불 요청에 대해 문의드립니다..")
+    @NotBlank(message = "본문은 필수입니다.")
+    private String content;
+}
+
+
