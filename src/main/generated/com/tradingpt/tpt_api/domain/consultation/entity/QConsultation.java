7c79108a8076626a2eef3e331233dffa3ef48afb
이승주 <eun07213@gmail.com>
2025-10-09 02:25:54 +0900

    feat: 어드민 민원 컨트롤러에 답변 관련 메서드 추가

---
+++ src/main/generated/com/tradingpt/tpt_api/domain/consultation/entity/QConsultation.java
@@ -0,0 +1,59 @@
+package com.tradingpt.tpt_api.domain.consultation.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QConsultation is a Querydsl query type for Consultation
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QConsultation extends EntityPathBase<Consultation> {
+
+    private static final long serialVersionUID = 646254728L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QConsultation consultation = new QConsultation("consultation");
+
+    public final DatePath<java.time.LocalDate> consultationDate = createDate("consultationDate", java.time.LocalDate.class);
+
+    public final TimePath<java.time.LocalTime> consultationTime = createTime("consultationTime", java.time.LocalTime.class);
+
+    public final com.tradingpt.tpt_api.domain.user.entity.QCustomer customer;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final BooleanPath isProcessed = createBoolean("isProcessed");
+
+    public final StringPath memo = createString("memo");
+
+    public QConsultation(String variable) {
+        this(Consultation.class, forVariable(variable), INITS);
+    }
+
+    public QConsultation(Path<? extends Consultation> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QConsultation(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QConsultation(PathMetadata metadata, PathInits inits) {
+        this(Consultation.class, metadata, inits);
+    }
+
+    public QConsultation(Class<? extends Consultation> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.customer = inits.isInitialized("customer") ? new com.tradingpt.tpt_api.domain.user.entity.QCustomer(forProperty("customer"), inits.get("customer")) : null;
+    }
+
+}
+
