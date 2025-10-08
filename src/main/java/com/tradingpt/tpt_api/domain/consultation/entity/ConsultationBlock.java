207477f6845d6f7e69c050d6f9892e792001039c
이승주 <eun07213@gmail.com>
2025-10-09 02:56:42 +0900

    feat: 상담 블록 엔티티 추가

---
+++ src/main/java/com/tradingpt/tpt_api/domain/consultation/entity/ConsultationBlock.java
@@ -0,0 +1,31 @@
+package com.tradingpt.tpt_api.domain.consultation.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ConsultationBlock {
+
+    @Id @
+            GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate consultationBlockDate;
+
+    private LocalTime consultationBlockTime;
+
+}
+
