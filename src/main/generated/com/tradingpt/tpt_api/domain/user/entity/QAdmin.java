d1813e8c14ae8e7a96374e104d1c9d85b87432d6
이승주 <eun07213@gmail.com>
2025-10-14 22:14:30 +0900

    fix: 트레이너 필드 변경에 따른 클래스 변경

---
+++ src/main/generated/com/tradingpt/tpt_api/domain/user/entity/QAdmin.java
@@ -0,0 +1,77 @@
+package com.tradingpt.tpt_api.domain.user.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QAdmin is a Querydsl query type for Admin
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QAdmin extends EntityPathBase<Admin> {
+
+    private static final long serialVersionUID = -1788088530L;
+
+    public static final QAdmin admin = new QAdmin("admin");
+
+    public final QUser _super = new QUser(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final StringPath email = _super.email;
+
+    //inherited
+    public final NumberPath<Long> id = _super.id;
+
+    //inherited
+    public final StringPath name = _super.name;
+
+    public final StringPath onelineIntroduction = createString("onelineIntroduction");
+
+    //inherited
+    public final StringPath password = _super.password;
+
+    //inherited
+    public final ListPath<PasswordHistory, QPasswordHistory> passwordHistories = _super.passwordHistories;
+
+    public final StringPath phoneNumber = createString("phoneNumber");
+
+    //inherited
+    public final StringPath profileImageKey = _super.profileImageKey;
+
+    //inherited
+    public final StringPath profileImageUrl = _super.profileImageUrl;
+
+    //inherited
+    public final EnumPath<com.tradingpt.tpt_api.domain.user.enums.Provider> provider = _super.provider;
+
+    //inherited
+    public final StringPath providerId = _super.providerId;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    //inherited
+    public final StringPath username = _super.username;
+
+    public QAdmin(String variable) {
+        super(Admin.class, forVariable(variable));
+    }
+
+    public QAdmin(Path<? extends Admin> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QAdmin(PathMetadata metadata) {
+        super(Admin.class, metadata);
+    }
+
+}
+
