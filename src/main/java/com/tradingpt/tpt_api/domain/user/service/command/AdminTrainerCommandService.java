5c68ce0c62b55d413879a0a56ca6cbbc8d41005d
이승주 <eun07213@gmail.com>
2025-10-15 01:14:27 +0900

    feat : 어드민 커맨드 서비스 추가

---
+++ src/main/java/com/tradingpt/tpt_api/domain/user/service/command/AdminTrainerCommandService.java
@@ -0,0 +1,11 @@
+package com.tradingpt.tpt_api.domain.user.service.command;
+
+import com.tradingpt.tpt_api.domain.user.dto.request.TrainerRequestDTO;
+import com.tradingpt.tpt_api.domain.user.dto.response.TrainerResponseDTO;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface AdminTrainerCommandService {
+    TrainerResponseDTO createTrainer(TrainerRequestDTO req, MultipartFile profileImage);
+    TrainerResponseDTO updateTrainer(Long trainerId, TrainerRequestDTO req, MultipartFile profileImage);
+    void deleteTrainer(Long trainerId);
+}
