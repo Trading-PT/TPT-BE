c1e1d63b3df5dc0c1a0ee17934543ef3289683f7
이승주 <eun07213@gmail.com>
2025-10-16 17:06:47 +0900

    feat : 유저 에러 상태 추가

--- src/main/java/com/tradingpt/tpt_api/domain/user/exception/UserErrorStatus.java
+++ src/main/java/com/tradingpt/tpt_api/domain/user/exception/UserErrorStatus.java
@@ -27,7 +27,9 @@ public enum UserErrorStatus implements BaseCodeInterface {
 	INVALID_STATUS_CHANGE(HttpStatus.BAD_REQUEST, "USER4008", "잘못된 상태 요청입니다."),
 	COURSE_STATUS_NOT_FOUND(HttpStatus.NOT_FOUND, "USER4009", "유저의 완강 여부가 존재하지 않습니다."),
 	PASSWORD_CONFIRM_NOT_MATCH(HttpStatus.BAD_REQUEST, "USER4010", "비밀번호가 일치하지 않습니다"),
-	LOGIN_ID_DUPLICATED(HttpStatus.BAD_REQUEST, "USER4011", "이미 존재하는 id입니다");
+	LOGIN_ID_DUPLICATED(HttpStatus.BAD_REQUEST, "USER4011", "이미 존재하는 id입니다"),
+	HAS_ASSIGNED_CUSTOMERS(HttpStatus.BAD_REQUEST, "USER4012", "해당 트레이너에게 배정된 고객이 존재합니다.");
+
 
 	private final HttpStatus httpStatus;
 	private final boolean isSuccess = false;
