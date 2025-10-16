57f87eb04dc936d824d4e9485d15d73bcacecb96
이승주 <eun07213@gmail.com>
2025-10-16 17:06:37 +0900

    feat : Customer 관련 수정

--- src/main/java/com/tradingpt/tpt_api/domain/user/repository/CustomerRepository.java
+++ src/main/java/com/tradingpt/tpt_api/domain/user/repository/CustomerRepository.java
@@ -1,5 +1,6 @@
 package com.tradingpt.tpt_api.domain.user.repository;
 
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,9 @@ import com.tradingpt.tpt_api.domain.user.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long>, CustomerRepositoryCustom {
 	Page<Customer> findByAssignedTrainer_Id(Long trainerId, Pageable pageable);
+
+	// 트레이너에 배정된 고객 리스트 조회
+	List<Customer> findByAssignedTrainer_Id(Long trainerId);
+
+	boolean existsByAssignedTrainer_Id(Long trainerId);
 }
