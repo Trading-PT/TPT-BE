2c0daf8a35f2e2485b89f9d6a026da6694fc4505
이승주 <eun07213@gmail.com>
2025-10-19 00:45:54 +0900

    feat : 레벨테스트 레포지토리 추가

---
+++ src/main/java/com/tradingpt/tpt_api/domain/leveltest/repository/LeveltestQuestionRepository.java
@@ -0,0 +1,7 @@
+package com.tradingpt.tpt_api.domain.leveltest.repository;
+
+import com.tradingpt.tpt_api.domain.leveltest.entity.LeveltestQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LeveltestQuestionRepository extends JpaRepository<LeveltestQuestion, Long> {
+}
