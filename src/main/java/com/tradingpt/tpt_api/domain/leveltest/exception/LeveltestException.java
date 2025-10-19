8cca752fa2f61ce564a0e88584dee13a39f7b5eb
이승주 <eun07213@gmail.com>
2025-10-20 04:09:58 +0900

    fix: 레벨테스트 예외 추가

--- src/main/java/com/tradingpt/tpt_api/domain/leveltest/exception/LeveltestException.java
+++ src/main/java/com/tradingpt/tpt_api/domain/leveltest/exception/LeveltestException.java
@@ -3,8 +3,8 @@ package com.tradingpt.tpt_api.domain.leveltest.exception;
 import com.tradingpt.tpt_api.global.exception.BaseException;
 import com.tradingpt.tpt_api.global.exception.code.BaseCodeInterface;
 
-public class LeveltestQuestionException extends BaseException {
-    public LeveltestQuestionException(BaseCodeInterface errorCode) {
+public class LeveltestException extends BaseException {
+    public LeveltestException(BaseCodeInterface errorCode) {
         super(errorCode);
     }
 }
