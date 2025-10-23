e7b883409c7949eee72e9a756adab7893dc7aa66
이승주 <eun07213@gmail.com>
2025-10-23 20:18:48 +0900

    feat: 레벨테스트 쿼리 서비스 추가

--- src/main/java/com/tradingpt/tpt_api/domain/leveltest/service/query/LeveltestQueryService.java
+++ src/main/java/com/tradingpt/tpt_api/domain/leveltest/service/query/LeveltestQueryService.java
@@ -4,8 +4,19 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 import com.tradingpt.tpt_api.domain.leveltest.dto.response.LevelTestQuestionUserResponseDTO;
+import com.tradingpt.tpt_api.domain.leveltest.dto.response.LeveltestAttemptDetailResponseDTO;
+import com.tradingpt.tpt_api.domain.leveltest.dto.response.LeveltestAttemptListResponseDTO;
+
+import java.util.List;
 
 public interface LeveltestQueryService {
 
+	// 문제 리스트 조회 (기존)
 	Slice<LevelTestQuestionUserResponseDTO> getQuestions(Pageable pageable);
+
+	// 채점 완료된 시도 조회
+	List<LeveltestAttemptListResponseDTO> getGradedAttempts(Long customerId);
+
+	// 특정 시도 상세 조회
+	LeveltestAttemptDetailResponseDTO getAttemptDetail(Long attemptId);
 }
