184a2c9891f840f77fc8bc47a2368bd28c4c78cc
dong99u <qkrehdrb0813@gmail.com>
2025-10-28 19:50:38 +0900

    :sparkles: feat: 연도별 월 리스트 조회 API 추가
    - 연도 및 고객 ID로 피드백 요청이 존재하는 월을 조회하는 메서드 추가
    - `@AuthenticationPrincipal` 어노테이션을 활용한 트레이너 검증 로직 추가 예정
    - 관련 엔드포인트 및 DTO 정의

--- src/main/java/com/tradingpt/tpt_api/domain/monthlytradingsummary/controller/AdminMonthlyTradingSummaryV1Controller.java
+++ src/main/java/com/tradingpt/tpt_api/domain/monthlytradingsummary/controller/AdminMonthlyTradingSummaryV1Controller.java
@@ -1,14 +1,18 @@
 package com.tradingpt.tpt_api.domain.monthlytradingsummary.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tradingpt.tpt_api.domain.feedbackrequest.dto.response.YearlySummaryResponseDTO;
 import com.tradingpt.tpt_api.domain.monthlytradingsummary.dto.request.CreateMonthlyTradingSummaryRequestDTO;
 import com.tradingpt.tpt_api.domain.monthlytradingsummary.service.command.MonthlyTradingSummaryCommandService;
+import com.tradingpt.tpt_api.domain.monthlytradingsummary.service.query.MonthlyTradingSummaryQueryService;
 import com.tradingpt.tpt_api.global.common.BaseResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -22,6 +26,7 @@ import lombok.RequiredArgsConstructor;
 @Tag(name = "ADMIN 월간 매매 일지 통계", description = "ADMIN이 사용하는 월간 매매 일지 통계 API")
 public class AdminMonthlyTradingSummaryV1Controller {
 
+	private final MonthlyTradingSummaryQueryService monthlyTradingSummaryQueryService;
 	private final MonthlyTradingSummaryCommandService monthlyTradingSummaryCommandService;
 
 	@Operation(summary = "해당 연/월에 대한 데이, 스윙 트레이딩 타입 피드백 통계",
@@ -37,6 +42,21 @@ public class AdminMonthlyTradingSummaryV1Controller {
 		return BaseResponse.onSuccessCreate(
 			monthlyTradingSummaryCommandService.createMonthlySummary(year, month, customerId, request)
 		);
+	}
+
+	@Operation(summary = "해당 연도에 대한 월 리스트",
+		description = "해당 연도에 대한 피드백 요청이 존재하는 월을 리스트업 합니다.")
+	@GetMapping("/customers/{customerId}/years/{year}")
+	@PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_TRAINER')")
+	public BaseResponse<YearlySummaryResponseDTO> getYearlySummaryResponse(
+		@PathVariable Integer year,
+		@PathVariable Long customerId,
+		@AuthenticationPrincipal(expression = "id") Long trainerId
+	) {
+		return BaseResponse.onSuccess(
+
+			// TODO: 내 담당 고객의 피드백만 볼 수 있도록 검증 로직을 추가해야 한다.
+			monthlyTradingSummaryQueryService.getYearlySummaryResponse(year, customerId));
 	}
 
 }
