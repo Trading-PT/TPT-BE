d1fba5a4fc738fbb49ced2806c41d17387105e25
이승주 <eun07213@gmail.com>
2025-10-30 23:53:29 +0900

    feat: 칼럼 관련 q클래스 추가

--- src/main/generated/com/tradingpt/tpt_api/domain/column/entity/QColumns.java
+++ src/main/generated/com/tradingpt/tpt_api/domain/column/entity/QColumns.java
@@ -22,17 +22,27 @@ public class QColumns extends EntityPathBase<Columns> {
 
     public static final QColumns columns = new QColumns("columns");
 
+    public final com.tradingpt.tpt_api.global.common.QBaseEntity _super = new com.tradingpt.tpt_api.global.common.QBaseEntity(this);
+
     public final QColumnCategory category;
 
     public final StringPath content = createString("content");
 
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
     public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final BooleanPath isBest = createBoolean("isBest");
 
     public final NumberPath<Integer> likeCount = createNumber("likeCount", Integer.class);
 
     public final StringPath subtitle = createString("subtitle");
 
     public final StringPath title = createString("title");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
 
     public final com.tradingpt.tpt_api.domain.user.entity.QUser user;
 
