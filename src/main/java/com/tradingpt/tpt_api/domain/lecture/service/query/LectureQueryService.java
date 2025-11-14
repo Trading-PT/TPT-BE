8938f52afed83f3de41bb912aaa3c31c0c6c2d2d
이승주 <eun07213@gmail.com>
2025-11-14 21:11:36 +0900

    feat: 유저 강의 쿼리 서비스 추가

---
+++ src/main/java/com/tradingpt/tpt_api/domain/lecture/service/query/LectureQueryService.java
@@ -0,0 +1,11 @@
+package com.tradingpt.tpt_api.domain.lecture.service.query;
+
+import com.tradingpt.tpt_api.domain.lecture.dto.response.ChapterBlockDTO;
+import com.tradingpt.tpt_api.domain.lecture.dto.response.LectureDetailDTO;
+import java.util.List;
+
+public interface LectureQueryService {
+    List<ChapterBlockDTO> getCurriculum(Long userId, int page, int size);
+    LectureDetailDTO getLectureDetail(Long userId, Long lectureId);
+}
+
