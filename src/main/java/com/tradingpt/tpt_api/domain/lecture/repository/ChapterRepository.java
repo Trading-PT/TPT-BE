35968433e645abb80a40d3167da114048f9ea495
이승주 <eun07213@gmail.com>
2025-11-17 00:27:26 +0900

    feat: 과제 관련 레포지토리 구현

--- src/main/java/com/tradingpt/tpt_api/domain/lecture/repository/ChapterRepository.java
+++ src/main/java/com/tradingpt/tpt_api/domain/lecture/repository/ChapterRepository.java
@@ -1,7 +1,15 @@
 package com.tradingpt.tpt_api.domain.lecture.repository;
 
+import com.tradingpt.tpt_api.domain.lecture.dto.response.ChapterListResponseDTO;
 import com.tradingpt.tpt_api.domain.lecture.entity.Chapter;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ChapterRepository extends JpaRepository<Chapter, Long> {
+    @Query("SELECT new com.tradingpt.tpt_api.domain.lecture.dto.response.ChapterListResponseDTO(" +
+            "c.id, c.title) " +
+            "FROM Chapter c " +
+            "ORDER BY c.chapterOrder ASC")
+    List<ChapterListResponseDTO> findAllSimple();
 }
