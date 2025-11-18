e7b06aafbf25b4ae0a1055d3669657442e5c7844
dong99u <qkrehdrb0813@gmail.com>
2025-11-18 15:53:02 +0900

    :sparkles: feat: QueryDSL Q 클래스 생성 및 리포지토리 로직 수정
    - AssignmentAttachment 및 CustomerAssignment에 대한 QueryDSL Q 클래스 생성
    - riskTaking 데이터 처리 로직을 BigDecimal로 수정하고 관련 연산 방식 변경

---
+++ src/main/generated/com/tradingpt/tpt_api/domain/lecture/entity/QAssignmentAttachment.java
@@ -0,0 +1,55 @@
+package com.tradingpt.tpt_api.domain.lecture.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QAssignmentAttachment is a Querydsl query type for AssignmentAttachment
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QAssignmentAttachment extends EntityPathBase<AssignmentAttachment> {
+
+    private static final long serialVersionUID = -979841678L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QAssignmentAttachment assignmentAttachment = new QAssignmentAttachment("assignmentAttachment");
+
+    public final QCustomerAssignment customerAssignment;
+
+    public final StringPath fileKey = createString("fileKey");
+
+    public final StringPath fileUrl = createString("fileUrl");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public QAssignmentAttachment(String variable) {
+        this(AssignmentAttachment.class, forVariable(variable), INITS);
+    }
+
+    public QAssignmentAttachment(Path<? extends AssignmentAttachment> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QAssignmentAttachment(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QAssignmentAttachment(PathMetadata metadata, PathInits inits) {
+        this(AssignmentAttachment.class, metadata, inits);
+    }
+
+    public QAssignmentAttachment(Class<? extends AssignmentAttachment> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.customerAssignment = inits.isInitialized("customerAssignment") ? new QCustomerAssignment(forProperty("customerAssignment"), inits.get("customerAssignment")) : null;
+    }
+
+}
+
