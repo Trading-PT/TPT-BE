5771b4237e477caf2afd4f6e0d15c95402c0d7c4
이승주 <eun07213@gmail.com>
2025-11-20 23:39:09 +0900

    fix: 매달 1일 상태 변경 스케줄러 수정

--- src/main/java/com/tradingpt/tpt_api/domain/lecture/scheduler/CourseStatusScheduler.java
+++ src/main/java/com/tradingpt/tpt_api/domain/lecture/scheduler/CourseStatusScheduler.java
@@ -5,6 +5,7 @@ import com.tradingpt.tpt_api.domain.user.enums.CourseStatus;
 import com.tradingpt.tpt_api.domain.user.repository.CustomerRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +18,11 @@ public class CourseStatusScheduler {
 
     // 매달 1일 새벽 1시
     @Scheduled(cron = "0 0 1 1 * *")
+    @SchedulerLock(
+            name = "courseStatusMonthlyJob",
+            lockAtLeastFor = "PT1M",
+            lockAtMostFor = "PT10M"
+    )
     @Transactional
     public void updateCompletedUsers() {
 
