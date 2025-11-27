779f4ba0df7bc4760206b20a90ab6ad2de0c4508
이승주 <eun07213@gmail.com>
2025-11-27 19:16:17 +0900

    feat: 강의 재생 API 관련 서비스 메서드 추가

--- src/main/java/com/tradingpt/tpt_api/domain/lecture/service/query/LectureQueryService.java
+++ src/main/java/com/tradingpt/tpt_api/domain/lecture/service/query/LectureQueryService.java
@@ -3,11 +3,13 @@ package com.tradingpt.tpt_api.domain.lecture.service.query;
 import com.tradingpt.tpt_api.domain.lecture.dto.response.AssignmentSubmissionDetailDTO;
 import com.tradingpt.tpt_api.domain.lecture.dto.response.ChapterBlockDTO;
 import com.tradingpt.tpt_api.domain.lecture.dto.response.LectureDetailDTO;
+import com.tradingpt.tpt_api.domain.lecture.dto.response.LecturePlayResponseDTO;
 import java.util.List;
 
 public interface LectureQueryService {
     List<ChapterBlockDTO> getCurriculum(Long userId, int page, int size);
     LectureDetailDTO getLectureDetail(Long userId, Long lectureId);
     AssignmentSubmissionDetailDTO getMyAssignmentDetail(Long userId, Long lectureId);
+    LecturePlayResponseDTO getLecturePlayUrl(Long userId, Long lectureId);
 }
 
