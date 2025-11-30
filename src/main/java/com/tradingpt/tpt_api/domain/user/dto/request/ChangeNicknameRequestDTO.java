d0d3091cc07f304bee2e22e37dd9c49bd4eee40f
이승주 <eun07213@gmail.com>
2025-12-01 01:32:38 +0900

    fix: 상태 수정

--- src/main/java/com/tradingpt/tpt_api/domain/user/dto/request/ChangeNicknameRequestDTO.java
+++ src/main/java/com/tradingpt/tpt_api/domain/user/dto/request/ChangeNicknameRequestDTO.java
@@ -2,10 +2,14 @@ package com.tradingpt.tpt_api.domain.user.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ChangeNicknameRequestDTO {
 
