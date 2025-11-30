ef7dd40874d3dae862809ae85a2f6dfb111b0503
이승주 <eun07213@gmail.com>
2025-11-30 16:15:38 +0900

    feat: 강의 레포지토리 추가

--- src/main/java/com/tradingpt/tpt_api/domain/lecture/repository/AssignmentAttachmentRepository.java
+++ src/main/java/com/tradingpt/tpt_api/domain/lecture/repository/AssignmentAttachmentRepository.java
@@ -1,10 +1,14 @@
 package com.tradingpt.tpt_api.domain.lecture.repository;
 
+import com.tradingpt.tpt_api.domain.lecture.dto.response.CustomerHomeworkSummaryResponseDTO;
 import com.tradingpt.tpt_api.domain.lecture.entity.AssignmentAttachment;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AssignmentAttachmentRepository extends JpaRepository<AssignmentAttachment, Long> {
-    Optional<AssignmentAttachment> findByCustomerAssignmentId(Long caId);
+    List<AssignmentAttachment> findAllByCustomerAssignmentIdOrderByAttemptNoDesc(Long customerAssignmentId);
+    int countByCustomerAssignmentId(Long customerAssignmentId);
+    List<AssignmentAttachment> findAllByCustomerAssignmentIdOrderByAttemptNoAsc(Long id);
 }
 
