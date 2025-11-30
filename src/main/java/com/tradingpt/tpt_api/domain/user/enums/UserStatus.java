eb980944d37b06af4709c6fe089419ed16f8fa00
이승주 <eun07213@gmail.com>
2025-11-30 21:02:35 +0900

    fix: UserStatus 수정

--- src/main/java/com/tradingpt/tpt_api/domain/user/enums/UserStatus.java
+++ src/main/java/com/tradingpt/tpt_api/domain/user/enums/UserStatus.java
@@ -4,5 +4,6 @@ public enum UserStatus {
 	UID_REVIEW_PENDING, // UID 검토 중
 	UID_APPROVED, // UID 검토 완료, 승인
 	UID_REJECTED, // UID 검토 완료, 거절
+	PAID_BEFORE_TRAINER_ASSIGNING, // 결제 완료, 트레이너 배정 전
 	TRAINER_ASSIGNED // 트레이너 배정 완료
 }
